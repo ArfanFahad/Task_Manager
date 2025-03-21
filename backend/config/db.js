04548e068560e6d582d399f50897e06427a3b00f
MdFahadFarazi <arfanfahad40@gmail.com>
2025-03-22 01:12:03 +0600

    Update comment in db.js for clarity

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,13 @@
-
-import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
-import dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
-import path from 'path';
-import { promises as fs } from 'fs';
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// listFiles is a function that I used to read the dir before adding
 // async function listFiles () {
 //     try {
 //         const files = await fs.readdir('.../');
@@ -23,20 +22,20 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST, 
-    database: process.env.DB_NAME, 
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 export const connectDB = async () => {
-    try {
-        await pool.connect();
-        console.log("Connected to PostgreSQL");
-    } catch (error) {
-        console.error("Database Connection Failed: ", error.message);
-    }
+  try {
+    await pool.connect();
+    console.log("➡ Connected to PostgreSQL");
+  } catch (error) {
+    console.error("Database Connection Failed: ", error.message);
+  }
 };
 
 export default pool;
