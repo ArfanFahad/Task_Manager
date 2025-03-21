80f89fdf86cbd5f3bfe068e4214ce50500b3a824
MdFahadFarazi <arfanfahad40@gmail.com>
2025-03-22 01:23:51 +0600

    feat: add UAParser to log user-agent details for incoming requests.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { UAParser } from "ua-parser-js";
 import taskRoutes from "./routes/taskRoutes.js";
 // import userRoutes from "./routes/userRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -8,21 +9,39 @@ const app = express();
 
 dotenv.config();
 
-// middleware 
+// middleware
 app.use(cors());
 app.use(express.json());
+// middleware to log details
+app.use((req, res, next) => {
+  const parser = new UAParser(req.headers["user-agent"]);
+  const result = parser.getResult();
 
+  console.log("Incoming Request: ");
+  console.log(`IP Address: ${req.ip}`);
+  console.log(`Browser: ${result.browser.name} ${result.browser.version}`);
+  console.log(`Operating System: ${result.os.name} ${result.os.version}`);
+  console.log(`Device: ${result.device.type || "PC"}`);
+  console.log(`Model: ${result.device.model || "N/A"}`);
+  console.log(
+    `Request URL: ${req.url} | Method: ${
+      req.method
+    } | Time: ${new Date().toLocaleTimeString()}`
+  );
+  console.log(`---------`);
 
-//routes 
+  next();
+});
+
+//routes
 app.use("/api/tasks", taskRoutes);
 // app.use("/api/users", userRoutes);
 
-
 //start server
-const PORT = process.env.PORT || 5000; 
-app.listen(PORT, async ()=> {
-    await connectDB();
-    console.log(`Server running on port ${PORT}`);
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, async () => {
+  await connectDB();
+  console.log(`Server running on port ${PORT}`);
+});
 
 export default app;
