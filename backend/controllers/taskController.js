f4b838e1b80cbee85a5797f3ca061e6fcf860f58
MdFahadFarazi <arfanfahad40@gmail.com>
2025-03-29 15:53:34 +0600

    feat: add task_status toggle API
    POST /tasks/:id/toggle
    - Toggles completion status (true/false)
    - Return new status or error

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,3 +1,4 @@
+import pool from "../config/db.js";
 import {
   getAllTasks,
   insertDataInDB,
@@ -55,5 +56,35 @@ export const removeTask = async (req, res) => {
   } catch (error) {
     console.error("Error deleting task: ", error);
     res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// Toggle Task Status
+export const toggleTaskStatus = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Getting the current status of the task
+    const { rows } = await pool.query(
+      "SELECT task_status FROM tasks WHERE id = $1",
+      [id]
+    );
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    const currentStatus = rows[0].task_status;
+    const newStatus = !currentStatus;
+
+    // Update the database
+    await pool.query("UPDATE tasks SET task_status = $1 WHERE id = $2", [
+      newStatus,
+      id,
+    ]);
+
+    res.json({ success: true, task_status: newStatus });
+  } catch (error) {
+    console.error("Error toggling task status: ", error);
+    res.status(500).json({ message: "Server error" });
   }
 };
