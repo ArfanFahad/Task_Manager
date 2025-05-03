0084636eab2073000b2fa93289ad3fcf18169362
Fahad <arfanfahad40@gmail.com>
2025-05-03 15:39:25 +0600

    feat(auth): add a new taskRoutes

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,3 +1,4 @@
+// /tasks, /tasks/:id - all protected
 import express, { Router } from "express";
 import {
   getTasks,
@@ -5,19 +6,23 @@ import {
   removeTask,
   editTask,
 } from "../controllers/taskController.js";
+import { authenticateUser } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 //Route to get all tasks
-router.get("/", getTasks);
+// router.get("/", getTasks);
 
 // Route to create a task
-router.post("/", createTask);
+// router.post("/", createTask);
 
 // Route to update a task
-router.put("/:id", editTask);
+// router.put("/:id", editTask);
 
 // Route to delete a task
-router.delete("/:id", removeTask);
+// router.delete("/:id", removeTask);
+
+// Authentication Route
+router.get("/", authenticateUser, getTasks);
 
 export default router;
