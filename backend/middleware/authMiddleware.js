c75e6d1821ae214288e3f828f75d547e4f6c84ad
Fahad <arfanfahad40@gmail.com>
2025-05-03 15:27:01 +0600

    feat: add JWT auth middleware for protected routes

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+// Checks JWT, adds req.user
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// console.log(__filename);
+// console.log(__dirname);
+
+dotenv.config({
+  path: path.resolve(__dirname, "../../.env"),
+});
+
+export const authenticateUser = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Access denied." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(403).json({ message: "Invalid token" });
+  }
+};
