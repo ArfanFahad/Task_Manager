694406fe0de83e867e1e559d52bf5ece2a2815fa
Fahad <arfanfahad40@gmail.com>
2025-05-07 13:10:21 +0600

    feat: make a way to send data to backend, still need improvement

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const registerForm = document.getElementById("register-form");
+  const loginFrom = document.getElementById("login-form");
 
   if (registerForm) {
     registerForm.addEventListener("submit", async (e) => {
@@ -23,7 +24,38 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(data);
 
         if (res.ok) {
+          localStorage.setItem("verifyEmail", email);
           window.location.href = "../views/verify.html";
+        } else {
+          message.textContent = data.message || "Something went wrong.";
+        }
+      } catch (error) {
+        message.textContent = "Network error.";
+      }
+    });
+  }
+
+  if (loginFrom) {
+    loginFrom.addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+
+      try {
+        const res = await fetch("http://localhost:5000/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        });
+
+        const data = await res.json();
+        console.log(data);
+
+        if (res.ok) {
+          window.location.href = "../views/dashboard.html";
         } else {
           message.textContent = data.message || "Something went wrong.";
         }
