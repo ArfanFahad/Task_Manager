3fd1ca40fc7ea6dc20ab873ab99573fa5f6f84d7
Fahad <arfanfahad40@gmail.com>
2025-05-17 12:59:47 +0600

    refactor: comment out some section for now

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,16 +1,17 @@
 // selecting elements
 import { fetchTasksFromDB, createTaskInDB, toggleTaskStatus } from "./api.js";
 import { addTaskToUI } from "./ui.js";
+import { showContent } from "./dashboard.js";
+import { ensureAuthenticated } from "./dashboard.js";
+showContent("dashboard"); // Default Loading Section
 
 const taskList = document.getElementById("taskList");
-const inputValue = document.getElementById("taskInput");
+// const inputValue = document.getElementById("taskInput");
 
 // Load tasks when the page loads
 document.getElementById("getTaskBtn").addEventListener("click", async () => {
   try {
     const tasks = await fetchTasksFromDB();
-
-    taskList.innerHTML = "";
 
     tasks.forEach((task) => {
       addTaskToUI(task);
@@ -21,26 +22,26 @@ document.getElementById("getTaskBtn").addEventListener("click", async () => {
 });
 
 // Event for taking the value
-document.getElementById("addTaskBtn").addEventListener("click", async () => {
-  const getValue = inputValue.value.trim();
-  try {
-    await createTaskInDB(getValue);
-    inputValue.value = "";
-  } catch (error) {
-    console.error("Error Passing Data: ", error);
-  }
-});
+// document.getElementById("addTaskBtn").addEventListener("click", async () => {
+//   const getValue = inputValue.value.trim();
+//   try {
+//     await createTaskInDB(getValue);
+//     inputValue.value = "";
+//   } catch (error) {
+//     console.error("Error Passing Data: ", error);
+//   }
+// });
 
 // Making task clickable and applying line-through on tasks
-document.addEventListener("DOMContentLoaded", () => {
-  taskList.addEventListener("click", async (event) => {
-    if (event.target.tagName === "SPAN") {
-      const taskId = event.target.dataset.id;
-      const updatedTask = await toggleTaskStatus(taskId);
+// document.addEventListener("DOMContentLoaded", () => {
+//   taskList.addEventListener("click", async (event) => {
+//     if (event.target.tagName === "SPAN") {
+//       const taskId = event.target.dataset.id;
+//       const updatedTask = await toggleTaskStatus(taskId);
 
-      if (updatedTask) {
-        event.target.classList.toggle("completed", updatedTask.task_status);
-      }
-    }
-  });
-});
+//       if (updatedTask) {
+//         event.target.classList.toggle("completed", updatedTask.task_status);
+//       }
+//     }
+//   });
+// });
