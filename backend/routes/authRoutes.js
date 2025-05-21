d1cbb5d811a24105879c0b4abf6664d7b513a4f8
Fahad <arfanfahad40@gmail.com>
2025-05-21 13:16:23 +0600

    feat: add /resend-code endpoint

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -11,6 +11,7 @@ import {
   registerUser,
   verifyUser,
   loginUser,
+  resendCode,
 } from "../controllers/authController.js";
 import express from "express";
 const router = express.Router();
@@ -23,5 +24,8 @@ router.post("/verify", verifyUser);
 
 // User Login
 router.post("/login", loginUser);
+
+// User Resend Verification
+router.post("/resend-code", resendCode);
 
 export default router;
