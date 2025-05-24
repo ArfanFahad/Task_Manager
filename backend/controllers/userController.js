3e220b4a9835a8cf854311d528c7af09e9059bb4
Fahad <arfanfahad40@gmail.com>
2025-05-24 23:15:45 +0600

    file initiated

---
+++ backend/controllers/userController.js
@@ -0,0 +1,9 @@
+/* 
+1. Get User Profile 
+2. Update user into (name, password)
+3. Delete user account 
+4. Change Password (while logged in)
+5. Upload/change profile picture 
+*/
+
+const getUserProfile = async (req, res) => {};
