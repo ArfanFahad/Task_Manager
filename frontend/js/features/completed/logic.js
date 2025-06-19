c809ac811e7b9859b3c79788572e43178d95b4f6
ArfanFahad <arfanfahad40@gmail.com>
2025-06-19 06:37:17 +0600

    feat: implement task completion toggle via click handler

---
+++ frontend/js/features/completed/logic.js
@@ -0,0 +1,29 @@
+import { toggleTaskStatus } from "../../api.js";
+
+// Making task clickable and applying line-through on tasks
+export const taskCompleted = () => {
+  const taskList = document.getElementById("taskList");
+
+  if (!taskList) {
+    console.warn("taskList element not found");
+    return;
+  }
+
+  taskList.addEventListener("click", async (event) => {
+    if (event.target.tagName === "SPAN") {
+      const taskId = event.target.dataset?.id;
+      if (!taskId) return;
+
+      try {
+        const updatedTask = await toggleTaskStatus(taskId);
+        if (!updatedTask) {
+          console.error(`Failed to update task with ID ${taskId}`);
+        } else {
+          event.target.classList.toggle("completed", updatedTask.task_status);
+        }
+      } catch (error) {
+        console.error("An error occured while updating task status");
+      }
+    }
+  });
+};
