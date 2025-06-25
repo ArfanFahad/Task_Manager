9633ebca2f66be792be8293109eae891fe210901
ArfanFahad <arfanfahad40@gmail.com>
2025-06-26 01:29:10 +0600

    fix: omit complex naming, make the function more readable and intuitive

--- frontend/js/features/allTasks/logic.js
+++ frontend/js/features/allTasks/logic.js
@@ -1,6 +1,6 @@
 import { addTaskToUI } from "../../ui.js";
 import { fetchTasksFromDB } from "../../api.js";
-import { taskCompleted } from "../completed/logic.js";
+import { taskCompleted, completedTaskLogic } from "../completed/logic.js";
 
 export const renderView = async () => {
   try {
@@ -9,5 +9,26 @@ export const renderView = async () => {
     taskCompleted(); // Line Through Feature
   } catch (error) {
     console.error("Error occured: ", error.message);
+  }
+};
+
+export const renderCompletedTask = async () => {
+  try {
+    const tasks = await completedTaskLogic();
+    const list = document.getElementById("completedTasks");
+    list.innerHTML = "";
+
+    if (tasks.length === 0) {
+      list.innerHTML = "<li>No completed tasks found.</li>";
+      return;
+    }
+
+    tasks.forEach((task) => {
+      const li = document.createElement("li");
+      li.textContent = `${task.task_name}`;
+      list.appendChild(li);
+    });
+  } catch (error) {
+    console.error("Error occurred: ", error.message);
   }
 };
