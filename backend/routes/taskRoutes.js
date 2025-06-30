2fdb34e8d6719704bb6f813905409e3e9e398e9b
ArfanFahad <arfanfahad40@gmail.com>
2025-06-30 13:10:12 +0600

    feat(routes): add protected route to fetch single user's pending tasks

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -8,6 +8,7 @@ import {
   getTaskSummary,
   getTaskStats,
   getCompletedTasks,
+  getPendingTasks,
 } from "../controllers/taskController.js";
 import { authenticateUser } from "../middleware/authMiddleware.js";
 
@@ -22,7 +23,10 @@ router.get("/user", authenticateUser, getTaskStats);
 // Protected API for Single User Completed Tasks
 router.get("/completed", authenticateUser, getCompletedTasks);
 
-//Route to get all tasks
+// Protected API for Single User Pending Tasks
+router.get("/pending", authenticateUser, getPendingTasks);
+
+// Route to get all tasks
 router.get("/", authenticateUser, getTasks);
 
 // Route to create a task
