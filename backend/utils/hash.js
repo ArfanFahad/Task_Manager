dc5cb1a407a085e53696e295a0822e518122dbbb
Arfan Fahad <arfanfahad40@gmail.com>
2025-09-14 11:32:29 +0600

    refactor: make code more readable with better comment

--- backend/utils/hash.js
+++ backend/utils/hash.js
@@ -1,9 +1,14 @@
 import bcryptjs from "bcryptjs";
 
 export async function hashPassword(password) {
+  /* Here, password is the normal password but 10
+  is the salt round. round means lock, so 1 is 1 lock
+  and 10 is 10 lock, means more protecttion */
   return await bcryptjs.hash(password, 10);
 }
 
 export async function comparePassword(password, hashedPassword) {
+  /* Here, bcryptjs compare two password, plan password with
+  hashed password and it returns true or false. */
   return await bcryptjs.compare(password, hashedPassword);
 }
